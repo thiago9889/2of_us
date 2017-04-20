cf3d016bfea120b2a81f9cbc080fb3046eb02dfd
Thiago Prado <thiago9889@gmail.com>
2017-04-20 16:32:30 +0000

    Programado front nas paginas de login e cadastro

--- index.php
+++ index.php
@@ -1,18 +1,35 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
   <head>
     <meta charset="UTF-8">
-    <title>Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Login - 2of us</title>
   </head>
+
   <body>
-    <h1>Acesse sua conta</h1>
-    <form method="post" id="formLogin">
-      <label for="email">E-mail</label><br>
-      <input type="email" maxlength="45" size="50" name="email" id="email" required autofocus><br>
-      <label for="senha">Senha</label><br>
-      <input type="password" maxlength="45" size="50" name="senha" id="senha" required><br><br>
-    </form>
-      <button type="submit" form="formLogin">Entrar</button>
-    <p>Não possui uma conta? <a href="cadastro.php">Cadastre-se</a> e conecte-se ao seu parceiro!</p>
+    <main class="text-center">
+      <img src="img/logo2ofus.png" alt="Logo 2 of us" class="logo" />
+      <h1>Acesse sua conta</h1>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4"></div>
+          <div class="col-md-4">
+            <form method="post" id="formLogin">
+              <label for="email">E-mail</label><br>
+              <input type="email" maxlength="45" name="email" id="email" required autofocus><br>
+              <label for="senha">Senha</label><br>
+              <input type="password" maxlength="45" name="senha" id="senha" required><br><br>
+              <button type="submit" class="btn_2ofus" form="formLogin">Entrar</button>
+            </form>
+          </div>
+        </div>
+        <p>Não possui uma conta? <a href="cadastro.php">Cadastre-se</a> e conecte-se ao seu parceiro!</p>
+      </div>
+    </main>
   </body>
 </html>
