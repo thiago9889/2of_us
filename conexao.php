b0226cfc186ebc49bd20aeedf7bfdcaa1cd766b2
beatriz_rodrigues21 <beatriz_rodrigues21@hotmail.com>
2017-04-23 17:25:50 -0400

    Estrutura dos formularios html das pags index.php e cadastro.php e html da pag conexao.php

--- conexao.php
+++ conexao.php
@@ -6,5 +6,12 @@
   </head>
   <body>
     <h1>Conecte-se ao seu parceiro</h1>
+    <p>Pronto para integrar sua conta? Compartilhe com seu parceiro o código abaixo:</p>
+    <input type="text" name="codigo" id="codigo" value="54325" disabled><br><br>
+    <button type="submit">Enviar código</button>
+    
+    <p>Já possui o código do parceiro? Digite abaixo:</p>
+    <input type="text" name="codigo" id="codigo" size="25" placeholder="Código de conexão do parceiro"><br><br>
+    <button type="submit">Confirmar</button>
   </body>
 </html>
